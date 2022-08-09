651cc6ce8bf3af85922976ff2093c5fa0b55a75b
kiruthika <r.kiruthika@capgemini.com>
2022-08-10 00:04:06 +0530

    Added Product Listing Card Style

--- congaaemreact/ui.frontend/src/components/Product/Product.js
+++ congaaemreact/ui.frontend/src/components/Product/Product.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {MapTo} from '@adobe/aem-react-editable-components';
 import productsData from '../../products.json';
 
+// include product style file
+require('./Product.scss');
+
 export const ProductEditConfig = {
 
     emptyLabel: 'Product',
@@ -29,19 +32,53 @@ export default class Product extends Component {
 //            </div>
 
 
-            <div className="product-card">
-                {
-                productsData.map(record => {
-                    return(
-                        <div className="product-box">
-                            {record.Name}
-                            {record.ProductCode}
-                            <img class="product-img" src={record.ImageURL}/>
+//            <div className="product-card">
+//                {
+//                productsData.map(record => {
+//                    return(
+//                        <div className="product-box">
+//                            {record.Name}
+//                            {record.ProductCode}
+//                            <img class="product-img" src={record.ImageURL}/>
+//                        </div>
+//                    )
+//                })
+//                }
+//            </div>
+
+
+        <div className="wrapper">
+        {
+          productsData && productsData.map(record => {
+            return(
+                <div className="card" key={ record.Id }>
+                <div className="card__body">
+                    <h5 className="card__title">{record.Name}</h5>
+                    <img src={record.ImageURL}
+                    class="card__image"/>
+                    <p className="card__description">Standard Price</p>
+
+
+                    { record.Prices && record.Prices.map(data => {
+                        return(
+                        <div className="card__price" key={ record.Id }>
+                            ${data.ListPrice}
                         </div>
-                    )
-                })
-                }
-            </div>
+                        )
+                    })}
+
+                </div>
+                <hr/>
+                <div class="align-items-center d-flex justify-content-center input-group-sm ng-star-inserted">
+                <label class="mr-3">Quantity</label>
+                <input type="number" min="1" name="quantity" class="form-control w-25"/>
+                </div>
+                <button className="card__btn">Add to Cart</button>
+                </div>
+            )
+        })
+        }
+        </div>
 
 
         );
